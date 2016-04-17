e219b01d71533b3a6c4c6ebbd90a32f743cf729d
Harvnlenny <harvnlenny@gmail.com>
2016-04-17 10:58:33 -0400

    edited the mainfest.js file to match our current project instead of just copy paste and ran gulp

--- js/manifest.js
+++ js/manifest.js
@@ -1,2 +1,2 @@
-// bootstrap bower package scripts
-//= include ../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js
+// MDL bower package scripts
+//= include ../bower_components/material-design-lite/material.min.js
