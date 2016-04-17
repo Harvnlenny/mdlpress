a87dedc56ac38d249e5517b25ab9d6dce5f67dd4
Harvnlenny <harvnlenny@gmail.com>
2016-04-17 11:42:18 -0400

    added minified version of scripts to our scripts in functions.php

--- functions.php
+++ functions.php
@@ -120,6 +120,9 @@ function mdlpress_scripts() {
 
 	wp_enqueue_script( 'mdlpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+		wp_enqueue_script( 'mdl-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array(), '1.0.0', true );
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
